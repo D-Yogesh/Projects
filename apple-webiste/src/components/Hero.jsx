afee695b6d091e27e3fe5ebd1c04dba55963ed8d
Yogesh Dollin <dollin.ys@gmail.com>
2024-03-23 18:49:19 +0530

    Add navbar and implement hero section.

--- apple-webiste/src/components/Hero.jsx
+++ apple-webiste/src/components/Hero.jsx
@@ -1,8 +1,44 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { useState } from "react"
+import { heroVideo, smallHeroVideo } from "../utils"
+import { useEffect } from "react"
+
 const Hero = () => {
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+    const handleWindowResize = () => {
+        console.log(window.innerWidth < 760);
+        setVideoSrc(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize)
+        }
+    }, [])
+
+    useGSAP(() => {
+        gsap.to('.hero-title', {opacity: 1, delay: 1.5})
+        gsap.to('#cta', {opacity: 1, y: -50, delay: 2.5})
+    }, [])
+    console.log(videoSrc)
     return (
-        <div>
-            Hero
-        </div>
+        <section className="w-full nav-height relative">
+            <div className="h-5/6 w-full flex-center flex-col">
+                <p className="hero-title">iPhone 15 Pro</p>
+                <div className="md:w-10/12 w-9/12">
+                    <video className="pointer-events-none" autoPlay muted key={videoSrc}>
+                        <source src={videoSrc} type="video/mp4"/>
+                    </video>
+                </div>
+            </div>
+            <div id="cta" className="flex flex-col items-center translate-y-20 opacity-0">
+                <a href="#highlights" className="btn">Buy</a>
+                <p className="font-normal text-xl">From $199/month or $999</p>
+            </div>
+        </section>
     )
 }
 export default Hero
--- apple-webiste/src/components/Navbar.jsx
+++ apple-webiste/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+import { navLists } from "../constants"
 import { appleImg, bagImg, searchImg } from "../utils"
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <header className="w-full py-5 sm:px-10 px-5">
+            <nav className="flex w-full screen-max-width justify-between">
+                <img src={appleImg} alt="apple icon" width={14} height={18}/>
+                <div className="flex flex-1 justify-center max-sm:hidden items-center">
+                    {navLists.map(nav => (
+                        <div key={nav} className="px-5 text-sm text-gray cursor-pointer hover:text-white transition-all">
+                            {nav}
+                        </div>
+                    ))}
+                </div>
+                <div className="flex gap-7 max-sm:justify-end max-sm:flex-1">
+                    <img src={searchImg} alt="search" width={18} height={18}/>
+                    <img src={bagImg} alt="bag" width={18} height={18}/>
+                </div>
+            </nav>
+        </header>
     )
 }
 export default Navbar
